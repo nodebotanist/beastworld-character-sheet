fc4743137b944b537c3f90ea9944db6b7bac2066
Kassian Wren <the@nodebotani.st>
2023-05-06 11:10:53 -0500

    moved main state to top, passed down props for StatsPropsLevelsBlock

--- components/character-sheet.tsx
+++ components/character-sheet.tsx
@@ -11,12 +11,38 @@ import StatsProfsLevelsBlock from './stats-profs-levels-block'
 export default class CharacterSheet extends Component {
     constructor (props) {
         super(props)
+        this.state = {
+            str: props.str,
+            dex: props.dex,
+            con: props.con,
+            int: props.int,
+            wis: props.wis,
+            cha: props.cha,
+            skills: props.skills,
+            name: props.name,
+            levels: props.levels,
+            profBonus: props.profBonus,
+            inspiration: props.inspiration,
+            passivePerception: props.passivePerception,
+        }
     }
 
     render() {
         return <div className="character-sheet-background">
             <CharacterInfoBlock />
-            <StatsProfsLevelsBlock />
+            <StatsProfsLevelsBlock 
+                str={this.state.str}
+                con={this.state.con}
+                int={this.state.int}
+                wis={this.state.wis}
+                cha={this.state.cha}
+                skills={this.state.skills}
+                name={this.state.name}
+                levels={this.state.levels}
+                profBonus={this.state.profBonus}
+                inspiration={this.state.inspiration}
+                passivePerception={this.state.passivePerception}
+            />
             <HealthBlock />
             <ACEtcBlock />
             <CharacterPortraitBlock />

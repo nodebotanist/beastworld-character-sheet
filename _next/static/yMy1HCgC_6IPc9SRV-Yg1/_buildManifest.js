177c22ce20e296de6b3cbb08f0776d619ca0040d
nodebotanist <the@nodebotani.st>
2023-05-13 15:29:28 +0000

    Deploying to gh-pages from @ nodebotanist/beastworld-character-sheet@d66deb5e168ec81e9ec05ca0ac309c6cc4d3d8de 🚀

---
+++ _next/static/yMy1HCgC_6IPc9SRV-Yg1/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-1f02ded13f457c1b.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

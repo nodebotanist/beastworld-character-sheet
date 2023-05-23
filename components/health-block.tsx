ebbca15a6a11cdf5da8e77a4386298265b6c424a
Kassian Wren <the@nodebotani.st>
2023-05-23 06:36:19 -0500

    Working on #11: Health block functionalized

--- components/health-block.tsx
+++ components/health-block.tsx
@@ -1,28 +1,15 @@
-import {Component, ReactPropTypes} from "react"
+import React, { useState } from 'react'
 
-export default class HealthBlock extends Component {
-    constructor(props: ReactPropTypes) {
-        super(props)
-        console.log(props)
-        this.state = {
-            currentHP: props.currentHP,
-            maxHP: props.maxHP,
-            tempHP: props.tempHP,
-        }
-    }
+const HealthBlock = ({ currentHP, maxHP, tempHP }) => {
+    const [ currentHPNum, setCurrentHPNum ] = useState(currentHP)
+    const [ maxHPNum, setMaxHPNum ] = useState(maxHP)
+    const [ tempHPNum, setTempHPNum ] = useState(tempHP)
 
-    render () {
-        return <div id="hp-block">
-            <input type="number" min="-50" max="50" id="current-hp" value={this.state.currentHP} onChange={this.onChangeHealth.bind(this)} />
-            <input type="number" min="0" max="50" id="max-hp" value={this.state.maxHP} onChange={this.onChangeHealth.bind(this)} />
-            <input type="number" min="-50" max="50" id="temp-hp" value={this.state.tempHP} onChange={this.onChangeHealth.bind(this)} />
-        </div>
-    }
-
-    onChangeHealth(event) {
-        let field = event.target.id.replace('-hp', 'HP')
-        this.setState({
-            [field]: event.target.value
-        })
-    }
+    return <div id="hp-block">
+        <input type="number" min="-50" max="50" id="current-hp" value={currentHPNum} onChange={ (e) => setCurrentHPNum(e.target.value) } />
+        <input type="number" min="0" max="50" id="max-hp" value={maxHPNum} onChange={ (e) => setMaxHPNum(e.target.value) } />
+        <input type="number" min="-50" max="50" id="temp-hp" value={tempHPNum} onChange={ (e) => setTempHPNum(e.target.value) } />
+    </div>
 }
+
+export default HealthBlock

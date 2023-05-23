5ad55de59af92536623caa26b53eeaeaa635f4c6
Kassian Wren <the@nodebotani.st>
2023-05-23 06:41:53 -0500

    Working on #11: Money block functionalized

--- components/money-block.tsx
+++ components/money-block.tsx
@@ -1,30 +1,17 @@
-import {Component, ReactPropTypes} from "react"
+import React, { useState } from 'react'
 
-import './money-block.module.css'
+const MoneyBlock = ({ copper, silver, gold, platinum }) => {
+    const [copperAmt, setCopperAmt] = useState(copper)
+    const [silverAmt, setSilverAmt] = useState(silver)
+    const [goldAmt, setGoldAmt] = useState(gold)
+    const [platinumAmt, setPlatinumAmt] = useState(platinum)
 
-export default class MoneyBlock extends Component {
-    constructor(props: ReactPropTypes) {
-        super(props)
-        this.state = {
-            copper: props.copper,
-            silver: props.silver,
-            gold: props.gold,
-            platinum: props.platinum,
-        }
-    }
-    render() {
-        return <div id="money-block">
-            <input type="number" name="copper" id="copper" min={0} value={this.state.copper} onChange={this.onChangeMoney.bind(this)} />
-            <input type="number" name="silver" id="silver" min={0} value={this.state.silver} onChange={this.onChangeMoney.bind(this)} />
-            <input type="number" name="gold" id="gold" min={0} value={this.state.gold} onChange={this.onChangeMoney.bind(this)} />
-            <input type="number" name="platinum" id="platinum" min={0} value={this.state.platinum} onChange={this.onChangeMoney.bind(this)} />
-        </div>
-    }
-    
-    onChangeMoney(event) {
-        let type = event.target.id
-        this.setState({
-            [type]: event.target.value
-        })
-    }
+    return <div id="money-block">
+        <input type="number" name="copper" id="copper" min={0} value={copperAmt} onChange={ (e) => setCopperAmt(e.target.value) } />
+        <input type="number" name="silver" id="silver" min={0} value={silverAmt} onChange={ (e) => setSilverAmt(e.target.value) } />
+        <input type="number" name="gold" id="gold" min={0} value={goldAmt} onChange={ (e) => setGoldAmt(e.target.value) } />
+        <input type="number" name="platinum" id="platinum" min={0} value={platinumAmt} onChange={ (e) => setPlatinumAmt(e.target.value) } />
+    </div>
 }
+
+export default MoneyBlock

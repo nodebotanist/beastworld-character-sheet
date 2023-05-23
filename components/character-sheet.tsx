81806a8daebd645890cb9105cedfa7358276ccb2
Kassian Wren <the@nodebotani.st>
2023-05-23 07:18:40 -0500

    Finishes #11: CharacterSheet block and all existing block functionalized

--- components/character-sheet.tsx
+++ components/character-sheet.tsx
@@ -1,5 +1,6 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import Image from 'next/image'
+
 import HealthBlock from './health-block'
 import ACEtcBlock from './ac-etc-block'
 import CharacterPortraitBlock from './character-portrait-block'
@@ -8,112 +9,71 @@ import MoneyBlock from './money-block'
 import CharacterInfoBlock from './character-info-block'
 import BottomBlock from './bottom-block'
 import StatsProfsLevelsBlock from './stats-profs-levels-block'
-
-import bg from '../public/images/char-sheet.jpg'
 import NameCrewBlock from './name-crew-block'
 
-export default class CharacterSheet extends Component {
-    constructor (props) {
-        super(props)
-        this.state = {
-            str: props.str,
-            dex: props.dex,
-            con: props.con,
-            int: props.int,
-            wis: props.wis,
-            cha: props.cha,
-            skills: props.skills,
-            name: props.name,
-            levels: props.levels,
-            profBonus: props.profBonus,
-            inspiration: props.inspiration,
-            passivePerception: props.passivePerception,
-            maxHP: props.maxHP,
-            currentHP: props.currentHP,
-            tempHP: props.tempHP,
-            copper: props.copper,
-            silver: props.silver,
-            gold: props.gold,
-            platinum: props.platinum,
-            profAndLangs: props.profAndLangs,
-            equipment: props.equipment,
-            classFeatures: props.classFeatures,
-            traitsBackground: props.traitsBackground,
-            traitsSpecies: props.traitsSpecies,
-            traitsHomeland: props.traitsHomeland,
-            attacksAndSpells: props.attacksAndSpells,
-            AC: props.AC,
-            init: props.init,
-            walkingSpeed: props.walkingSpeed,
-            hitDice: props.hitDice,
-            species: props.species,
-            alignment: props.alignment,
-            homeland: props.homeland,
-            background: props.background,
-            crewName: props.crewName,
-            playerName: props.playerName,
-            portrait: props.portrait
-        }
-    }
+import bg from '../public/images/char-sheet.jpg'
 
-    render() {
-        return <div className="character-sheet-background">
-            <Image class="bg" src={bg} alt="Beastworld Character Sheet page 1" />
-            <NameCrewBlock
-                name={this.state.playerName}
-                crew={this.state.crewName}
-            />
-            <CharacterInfoBlock 
-                species={this.state.species}
-                alignment={this.state.alignment}
-                homeland={this.state.homeland}
-                background={this.state.background}    
-            />
-            <StatsProfsLevelsBlock 
-                str={this.state.str}
-                dex={this.state.dex}
-                con={this.state.con}
-                int={this.state.int}
-                wis={this.state.wis}
-                cha={this.state.cha}
-                skills={this.state.skills}
-                name={this.state.name}
-                levels={this.state.levels}
-                profBonus={this.state.profBonus}
-                inspiration={this.state.inspiration}
-                passivePerception={this.state.passivePerception}
-            />
-            <HealthBlock 
-                currentHP={this.state.currentHP}
-                maxHP={this.state.maxHP}
-                tempHP={this.state.tempHP}    
-            />
-            <ACEtcBlock
-                AC={this.state.AC}
-                init={this.state.init}
-                walkingSpeed={this.state.walkingSpeed}
-                hitDice={this.state.hitDice}
-            />
-            <CharacterPortraitBlock 
-                portrait={this.state.portrait}
-            />
-            <AttacksSpellcastingBlock 
-                attacksAndSpells={this.state.attacksAndSpells}
-            />
-            <MoneyBlock
-                copper={this.state.copper}
-                silver={this.state.silver}
-                gold={this.state.gold}
-                platinum={this.state.platinum}
-            />
-            <BottomBlock 
-                profAndLangs= {this.state.profAndLangs}
-                equipment= {this.state.equipment}
-                classFeatures= {this.state.classFeatures}
-                traitsBackground= {this.state.traitsBackground}
-                traitsSpecies= {this.state.traitsSpecies}
-                traitsHomeland= {this.state.traitsHomeland}
-            />
-        </div>
-    }
+const CharacterSheet = (props) => {
+    const [sheetState, setSheetState] = useState(props)
+
+    return <div className="character-sheet-background">
+        <Image class="bg" src={bg} alt="Beastworld Character Sheet page 1" />
+        <NameCrewBlock
+            name={sheetState.playerName}
+            crew={sheetState.crewName}
+        />
+        <CharacterInfoBlock 
+            species={sheetState.species}
+            alignment={sheetState.alignment}
+            homeland={sheetState.homeland}
+            background={sheetState.background}    
+        />
+        <StatsProfsLevelsBlock 
+            str={sheetState.str}
+            dex={sheetState.dex}
+            con={sheetState.con}
+            int={sheetState.int}
+            wis={sheetState.wis}
+            cha={sheetState.cha}
+            skills={sheetState.skills}
+            name={sheetState.name}
+            levels={sheetState.levels}
+            profBonus={sheetState.profBonus}
+            inspiration={sheetState.inspiration}
+            passivePerception={sheetState.passivePerception}
+        />
+        <HealthBlock 
+            currentHP={sheetState.currentHP}
+            maxHP={sheetState.maxHP}
+            tempHP={sheetState.tempHP}    
+        />
+        <ACEtcBlock
+            AC={sheetState.AC}
+            init={sheetState.init}
+            walkingSpeed={sheetState.walkingSpeed}
+            hitDice={sheetState.hitDice}
+        />
+        <CharacterPortraitBlock 
+            portrait={sheetState.portrait}
+        />
+        <AttacksSpellcastingBlock 
+            attacksAndSpells={sheetState.attacksAndSpells}
+        />
+        <MoneyBlock
+            copper={sheetState.copper}
+            silver={sheetState.silver}
+            gold={sheetState.gold}
+            platinum={sheetState.platinum}
+        />
+        <BottomBlock 
+            profAndLangs= {sheetState.profAndLangs}
+            equipment= {sheetState.equipment}
+            classFeatures= {sheetState.classFeatures}
+            traitsBackground= {sheetState.traitsBackground}
+            traitsSpecies= {sheetState.traitsSpecies}
+            traitsHomeland= {sheetState.traitsHomeland}
+        />
+    </div>
 }
+
+export default CharacterSheet

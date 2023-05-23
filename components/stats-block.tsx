6aca50cba76816156af0716f17e7d2644ae67e29
Kassian Wren <the@nodebotani.st>
2023-05-23 07:03:49 -0500

    Working on #11: Stats block functionalized

--- components/stats-block.tsx
+++ components/stats-block.tsx
@@ -1,55 +1,67 @@
-import {Component, ReactPropTypes} from "react"
+import React, { useState } from 'react'
 
-export default class StatsBlock extends Component {
-    constructor(props: ReactPropTypes) {
-        super(props)
-        this.state = {
-            str: props.str,
-            dex: props.dex,
-            con: props.con,
-            int: props.int,
-            wis: props.wis,
-            cha: props.cha,
-            str_mod: `+${Math.floor((props.str - 10)/2)}`,
-            dex_mod: `+${Math.floor((props.dex - 10)/2)}`,
-            con_mod: `+${Math.floor((props.con - 10)/2)}`,
-            int_mod: `+${Math.floor((props.int - 10)/2)}`,
-            wis_mod: `+${Math.floor((props.wis - 10)/2)}`,
-            cha_mod: `+${Math.floor((props.cha - 10)/2)}`,
+const StatsBlock = ({ str, dex, con, int, wis, cha }) => {
+    const [strength, setStrength] = useState(str)
+    const [strengthMod, setStrengthMod] = useState(`+${Math.floor((str - 10) / 2)}`)
+    const [dexterity, setDexterity] = useState(dex)
+    const [dexterityMod, setDexterityMod] = useState(`+${Math.floor((dex - 10) / 2)}`)
+    const [constitution, setConstitution] = useState(con)
+    const [constitutionMod, setConstitutionMod] = useState(`+${Math.floor((con - 10) / 2)}`)
+    const [intelligence, setIntelligence] = useState(int)
+    const [intelligenceMod, setIntelligenceMod] = useState(`+${Math.floor((int - 10) / 2)}`)
+    const [wisdom, setWisdom] = useState(wis)
+    const [wisdomMod, setWisdomMod] = useState(`+${Math.floor((wis - 10) / 2)}`)
+    const [charisma, setCharisma] = useState(cha)
+    const [charismaMod, setCharismaMod] = useState(`+${Math.floor((cha - 10) / 2)}`)
+
+    const onStatsChange = (event) => {
+        const statToUpdate = event.target.name
+        switch (statToUpdate) {
+            case 'str':
+                setStrength(event.target.value)
+                setStrengthMod(`+${Math.floor((event.target.value - 10) / 2)}`)
+                break;
+            case 'dex':
+                setDexterity(event.target.value)
+                setDexterityMod(`+${Math.floor((event.target.value - 10) / 2)}`)
+                break;
+            case 'con':
+                setConstitution(event.target.value)
+                setConstitutionMod(`+${Math.floor((event.target.value - 10) / 2)}`)
+                break;
+            case 'int': 
+                setIntelligence(event.target.value)
+                setIntelligenceMod(`+${Math.floor((event.target.value - 10) / 2)}`)
+                break;
+            case 'wis':
+                setWisdom(event.target.value)
+                setWisdomMod(`+${Math.floor((event.target.value - 10) / 2)}`)
+                break;
+            case 'cha':
+                setCharisma(event.target.value)
+                setCharismaMod(`+${Math.floor((event.target.value - 10) / 2)}`)
+                break;
         }
     }
 
-    render() {   
-        const { str, dex, con, int, wis, cha, str_mod, dex_mod, con_mod, int_mod, wis_mod, cha_mod } = this.state 
-        return <> 
-            <form id="stats" onSubmit={(e) => e.preventDefault()}>
-                <input type="number" id="stats-str" name="str" value={str} min="0" max="50" onChange={this.onStatsChange}/>
-                <input type="number" id="stats-dex" name="dex" value={dex} min="0" max="50" onChange={this.onStatsChange} />
-                <input type="number" id="stats-con" name="con" value={con} min="0" max="50" onChange={this.onStatsChange} />
-                <input type="number" id="stats-int" name="int" value={int} min="0" max="50" onChange={this.onStatsChange} />
-                <input type="number" id="stats-wis" name="wis" value={wis} min="0" max="50" onChange={this.onStatsChange} />
-                <input type="number" id="stats-cha" name="cha" value={cha} min="0" max="50" onChange={this.onStatsChange} />
-            </form>
-            <div id="modifiers">
-                <div id="modifiers-str">{str_mod}</div>
-                <div id="modifiers-dex">{dex_mod}</div>
-                <div id="modifiers-con">{con_mod}</div>
-                <div id="modifiers-int">{int_mod}</div>
-                <div id="modifiers-wis">{wis_mod}</div>
-                <div id="modifiers-cha">{cha_mod}</div>
-            </div>
-        </>
-    }
-
-    onStatsChange = (event) => {
-        let statToChange = event.target.id.split('-')[1]
-        let modifier = Math.floor((event.target.value - 10) / 2).toString()
-        let modifierKey = statToChange + "_mod"
-        modifier = parseInt(modifier, 10) >= 0 ? `+${modifier}`: modifier.toString()
-        this.setState({
-            [statToChange]: event.target.value,
-            [modifierKey]: modifier
-        })
-        
-    }
+    return <> 
+        <form id="stats" onSubmit={(e) => e.preventDefault()}>
+            <input type="number" id="stats-str" name="str" value={strength} min="0" max="50" onChange={onStatsChange}/>
+            <input type="number" id="stats-dex" name="dex" value={dexterity} min="0" max="50" onChange={onStatsChange} />
+            <input type="number" id="stats-con" name="con" value={constitution} min="0" max="50" onChange={onStatsChange} />
+            <input type="number" id="stats-int" name="int" value={intelligence} min="0" max="50" onChange={onStatsChange} />
+            <input type="number" id="stats-wis" name="wis" value={wisdom} min="0" max="50" onChange={onStatsChange} />
+            <input type="number" id="stats-cha" name="cha" value={charisma} min="0" max="50" onChange={onStatsChange} />
+        </form>
+        <div id="modifiers">
+            <div id="modifiers-str">{strengthMod}</div>
+            <div id="modifiers-dex">{dexterityMod}</div>
+            <div id="modifiers-con">{constitutionMod}</div>
+            <div id="modifiers-int">{intelligenceMod}</div>
+            <div id="modifiers-wis">{wisdomMod}</div>
+            <div id="modifiers-cha">{charismaMod}</div>
+        </div>
+    </>
 }
+
+export default StatsBlock
